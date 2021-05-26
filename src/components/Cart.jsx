582f66c0d949e370886a2d19abc3a37458868792
ranjitha <ranjitha.k@hummingwave.com>
2021-05-26 21:59:29 +0530

    -

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import { FavoriteBorder} from "@material-ui/icons";
 import { Clear } from "@material-ui/icons";
 import apiService from "./APIService"
